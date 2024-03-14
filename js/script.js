3ed1d567eb6242f67bf1b61f0c7f61f5411dca4b
AngeloC89 <Angelociulla89@gmail.com>
2024-03-14 17:20:13 +0100

    program work but not good - try to fix

--- js/script.js
+++ js/script.js
@@ -30,18 +30,35 @@ palindromeCheck(word);
 
 /***************************** Pari e Dispari ********************** */
 
-
+//scrivo le variabili che mi serviranno
 let num = parseInt(prompt('scegli un numero da 1 a 5:'));
-let oddEven = prompt('scrivi pari o dispari').toLowerCase;
-let pcNun = getRndInteger(1, 5);
-console.log(pcNun);
+let oddEven = prompt('scrivi pari o dispari');
+let pcNum = getRndInteger(1, 5);
+console.log('numero scelto dal PC', pcNum);
+console.log('numero scelto dall\'utente', num)
+console.log('scelta utente:', oddEven);
+let sum = num + pcNum;
+console.log('la somma è', sum);
 
-let sum = num + pcNun;
-console.log(sum);
+   //dico al programma che la scelta dell'utente deve essere pari o dispari
+    if ((oddEven !== 'pari') && (oddEven !== 'dispari')) {
+        console.log('valore inserito non valido.')
+    } else {
+//se òa scelte è valida prosegue con il confronto
+        if ((sum % 2 === 0 && oddEven === 'pari') || (sum % 2 !== 0 && oddEven === 'dispari')) {
+
+            console.log('Hai vinto!');
+        } else {
+
+            console.log('Hai perso!');
+        }
+    
+};
 
 
-if ((oddEven !== 'pari') || (oddEven !== 'dispari')) {
-    console.log('valore inserito non valido.')
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 
